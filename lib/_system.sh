a354e0216f1168f73260622c74fcb1cf45772aa4
Ronald Santos <62314324+ronald-b@users.noreply.github.com>
2024-12-15 22:23:18 -0400

    Update _system.sh

--- lib/_system.sh
+++ lib/_system.sh
@@ -38,7 +38,7 @@ system_git_clone() {
   sleep 2
 
 sudo su - deploy <<EOF
-  git clone https://${token_code}@github.com/ronald-b/whaticket.git /home/deploy/${instancia_add}
+  git clone https://${token_code}@github.com/ronald-b/whaticket45.git /home/deploy/${instancia_add}
 EOF
 
   sleep 2
